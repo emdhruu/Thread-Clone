dbbaf4f3236892df73fccda4d4dadd0fef077143
emdhruu <thakkardhruti22@gmail.com>
2024-09-15 01:14:30 +0530

    setup route and dark theme , colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Button, Container } from "@chakra-ui/react";
+
 function App() {
-  return <> hello world</>;
+  return (
+    <Container maxW="280px" mt={5}>
+      <Button>Submit</Button>
+    </Container>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,60 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  ColorModeScript,
+  extendTheme,
+  useStyleConfig,
+} from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import UserPages from "./pages/UserPages.jsx";
+import PostPages from "./pages/PostPages.jsx";
+
+const styles = {
+  global: (props) => ({
+    body: {
+      color: mode("gray.800", "whiteAlpha.900")(props),
+      bg: mode("gray.100", "#101010")(props),
+    },
+  }),
+};
+
+const config = {
+  initialColorMode: "dark",
+  useStyleColorMode: true,
+};
+
+const colors = {
+  gray: {
+    light: "#616161",
+    dark: "#1e1e1e",
+  },
+};
+
+const theme = extendTheme({ config, styles, colors });
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/:username",
+    element: <UserPages />,
+  },
+  {
+    path: "/:username/post/:p id",
+    element: <PostPages />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </StrictMode>
 );
